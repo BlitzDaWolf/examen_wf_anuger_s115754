e5a13cb3a2b0ec88659aa2502b8df30cfb7c846d
BlitzDaWolf <botjes2@hotmail.com>
2020-01-20 08:58:01 +0100

    Servies returns 2 products

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -6,6 +6,11 @@ export class ProductService {
 
    constructor() {}
 
-   
+   getAllProducts(){
+      return [
+         new Product("product1", "bran1", "een generieke product", 200),
+         new Product("product2", "bran2", "een generieke product", 200)
+      ]
+   }
 
 }
